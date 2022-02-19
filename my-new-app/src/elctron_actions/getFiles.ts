1fbec69b3505cb443be593d788eb28003cb2068a
E=mc2@6.18 <ali.alkaraawi@crowdbotics.com>
2022-02-19 10:52:39 +0300

    change the structure of the repo

--- my-new-app/src/elctron_actions/getFiles.ts
+++
@@ -1,5 +0,0 @@
-export const getFiles = () => {
-    const fs = window.require('fs')
-    const new_files = fs.readdirSync('./')
-    return new_files
-}
--- my-new-app/src/renderer.ts
+++
@@ -1,3 +0,0 @@
-// Add this to the end of the existing file
-
-import './index_app';
